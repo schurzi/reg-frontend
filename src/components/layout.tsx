678712f8630c131b995d7dcf30f9bc3c3c4722c5
Ruben Tytgat <ruben_tytgat@hotmail.com>
2022-12-11 16:03:43 +0100

    SSR-ready l10n

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,6 @@
-import { LocalizationProvider, ReactLocalization } from '@fluent/react'
-import { useObservable, useObservableState } from 'observable-hooks'
-
 import Header from './header'
 import Footer from './footer'
 import '@eurofurence/reg-component-library/dist/index.css'
-import { loadLanguage, useCurrentLangKey } from '~/localization'
-import { from } from 'rxjs'
-import { concatMap } from 'rxjs/operators'
 import type { DeepReadonly } from 'ts-essentials'
 import { ReadonlyDate } from '~/util/readonly-types'
 
@@ -15,18 +9,10 @@ export interface LayoutProps {
 	readonly children: DeepReadonly<React.ReactNode>
 }
 
-const Layout = ({ deadline, children }: LayoutProps) => {
-	const langKey = useCurrentLangKey()
-	const localization$ = useObservable(langKey$ => langKey$.pipe(concatMap(([l]) => from(loadLanguage(l)))), [langKey])
-	const localization = useObservableState(localization$, new ReactLocalization([]))
-
-	return <LocalizationProvider l10n={localization}>
-		<>
-			<Header deadline={deadline}/>
-			{children}
-			<Footer/>
-		</>
-	</LocalizationProvider>
-}
+const Layout = ({ deadline, children }: LayoutProps) => <>
+	<Header deadline={deadline}/>
+	{children}
+	<Footer/>
+</>
 
 export default Layout
