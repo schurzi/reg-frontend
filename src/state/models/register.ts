77df135b9c825279fbacf5fab76484ed7d54dc5b
Ruben Tytgat <ruben_tytgat@hotmail.com>
2023-03-09 18:37:39 +0100

    Retain unknown packages and flags

--- src/state/models/register.ts
+++ src/state/models/register.ts
@@ -83,6 +83,8 @@ export interface RegistrationInfo {
 	readonly contactInfo: ContactInfo
 	readonly optionalInfo: OptionalInfo
 	readonly personalInfo: PersonalInfo
+	readonly unknownFlags?: string
+	readonly unknownPackages?: string
 }
 
 export interface PaymentInfo {
